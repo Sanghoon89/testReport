03b561f48533fdd85086c0580bd3558130dbe757
Hooni <lmsh2355@naver.com>
2021-09-10 10:27:40 +0900

    event admin

--- Event/admin.py
+++ Event/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from Event.models import Log
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('backup_dt', 'schedule_tm', 'schedule_nm', 'nodename', 'domain_nm', 'start_tm', 'end_tm', 'taken', 'status', 'returncode', 'action', 'note') # 변경
+    list_filter = ('returncode',)
+    search_fields = ('schedule_nm', 'nodename')
