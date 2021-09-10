213306e280f2f0806364c5fdf1f36633942a8425
Hooni <lmsh2355@naver.com>
2021-09-10 10:28:41 +0900

    event views

--- Event/views.py
+++ Event/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView, DetailView
+from event.models import Log
+
+class LogLV(ListView):
+    model = Log
+
+class LogDV(DetailView):
+    model = Log
