9d4f699ef657dc858c8317f99ec36c918cd403f7
Hooni <lmsh2355@naver.com>
2021-09-12 16:49:22 +0900

    polls views

--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,5 @@ from polls.models import Question
 
 def index(request):
     latest_question_list = Question.object.all().order_by('-pub_date')[:5]
-    context = ['latest_question_list': latest_question_list]
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
