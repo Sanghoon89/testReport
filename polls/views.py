08cb62a2fee838dc8d1fe16a6ed357fdeb636501
문상훈 <hooni@interpark.com>
2021-09-13 10:24:46 +0900

    polls views

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,7 @@ def index(request):
     latest_question_list = Question.object.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+def detail(request):
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/detail.html', context)
