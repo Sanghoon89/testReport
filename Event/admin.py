a2ffc19f4f53e1a59bbd6510cd2cbd826b23aaee
Hooni <lmsh2355@naver.com>
2021-09-13 22:46:38 +0900

    admin event

--- Event/admin.py
+++ Event/admin.py
@@ -9,3 +9,5 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('backup_dt', 'schedule_tm', 'schedule_nm', 'nodename', 'domain_nm', 'start_tm', 'end_tm', 'taken', 'status', 'returncode', 'action', 'note') # 변경
     list_filter = ('returncode',)
     search_fields = ('schedule_nm', 'nodename')
+
+class LogInline(admin.TabularInline)
