984ea951badced9136d23b6f9ed12bff571d68ec
Hooni <lmsh2355@naver.com>
2021-09-13 21:58:29 +0900

    views

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    latest_question_list = Question.object.all().order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
