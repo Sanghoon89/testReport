8e08d713696b53b33383a209ec16d0a47938ad54
Hooni <lmsh2355@naver.com>
2021-09-27 23:08:18 +0900

    remote views

--- Remote/views.py
+++ Remote/views.py
@@ -18,7 +18,7 @@ class KeepLV(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(KeepLV, self).get_context_data(**kwargs)
-        context['bar_list'] = Keep.object.filter(due_dt='2021-09-30')
+        context['bar_list'] = Keep.objects.filter(due_dt='2021-09-30')
         return context
 
     def __str__ (self):
