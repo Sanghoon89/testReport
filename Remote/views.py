5438e0df55dda0d13faaf51bbc9e982acff7a797
Hooni <lmsh2355@naver.com>
2021-09-27 22:33:07 +0900

    remote view

--- Remote/views.py
+++ Remote/views.py
@@ -11,9 +11,9 @@ class KeepLV(ListView):
     context_object_name = 'keeps'
 
     def get_queryset(self):
-        checkin = keeps.objects.filter(due_dt='2021-09-30')
-        checkout = Keep.objects.filter(check_dt='2021-09-16')
-        return checkin, checkout
+        checkin = Keep.objects.filter(due_dt='2021-09-30')
+#        checkout = Keep.objects.filter(check_dt='2021-09-16')
+        return checkin #, checkout
 
     def __str__ (self):
         return self
