6a6d64d534acafd3a8ebf3064b90c9b64637c92d
Hooni <lmsh2355@naver.com>
2021-09-27 23:55:48 +0900

    keep_list.html

--- Remote/views.py
+++ Remote/views.py
@@ -16,6 +16,7 @@ class KeepLV(ListView):
         context['checkin'] = Keep.objects.filter(due_dt='2021-09-30')
         context['checkout'] = Keep.objects.filter(check_dt='2021-09-16')
         context['srcpool'] = Keep.objects.filter(pool_nm='srcpool', due_dt__gte='2021-09-16')
+        context['srcpool'] = srcpool.order_by('-due_dt')
         return context
 
     def __str__ (self):
