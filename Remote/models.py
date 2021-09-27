c6cfdec9479426e150fe284a0e1ae471812a4a1f
Hooni <lmsh2355@naver.com>
2021-09-27 16:23:48 +0900

    remote models

--- Remote/models.py
+++ Remote/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Keep(models.Model):
+    check_dt = models.DateField('Check-out Date')
+    volume_nm = models.CharField('Tape Volume Name', max_length=20)
+    pool_nm = models.CharField('Pool Name', max_length=20)
+    cycle = models.IntegerField('Keep Cycle', default=14)
+    due_dt = models.DateField('Due Date')
+    safein_chk = models.CharField('Check in coffer', max_length=20, default="X")
+    safeout_chk = models.CharField('Check out coffer', max_length=20, default="X")
+
+    class Meta:
+        verbose_name = 'Keep'
+        verbose_name_plural = 'keeps'
+        db_table = 'RemoteKeep'
+        ordering = ('-check_dt','pool_nm','volume_nm',)
+
+    def __str__(self):
+        return f'{self.schedule_tm} {self.schedule_nm} {self.nodename} {self.domain_nm} {self.start_tm}'
+#        return '{0} {1}'.format(self.schedule_tm, self.schedule_nm)
+
+    def __date__ (self):
+        return self.check_dt
