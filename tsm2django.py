fde68d0d5bbf27d2b13314aedc97a779d9a5d41d
문상훈 <hooni@interpark.com>
2021-09-29 08:56:27 +0900

    tsm2dja

--- tsm2django.py
+++ tsm2django.py
@@ -1,7 +1,7 @@
 #!/usr/service/VENV/bin/python
 
 import pymysql.cursors
-import requests, locale
+import locale
 
 from datetime import datetime, date, timedelta
 
@@ -10,64 +10,42 @@ locale.setlocale(locale.LC_ALL,'')
 YESTERDAY = date.today() - timedelta(1)
 DATE = YESTERDAY.strftime('%Y-%m-%d')
 
-conn = pymysql.connect(host='180.70.98.216', user='root', password='collect123$', db='tsmbackup', charset='utf8')
-curs = conn.cursor()
+SQL_1 = f"select * from tsmEventLog where Date = '{DATE}' ORDER BY Date, Domain, Scheduled "
+TableName_1 = "EventLog"
+Columns_1 = "backup_dt,schedule_tm,schedule_nm,nodename,domain_nm,start_tm,end_tm,taken,status,returncode,action,note,modify_dt"
+INSQL_1 = f"INSERT INTO {TableName_1} ({Columns_1}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '', '',now())"
 
-SQL = f"select * from tsmEventLog where Date = '{DATE}' ORDER BY Date, Domain, Scheduled "
 SQL_2 = f"select * from tsmRemoteKeep where check_date = '{DATE}' ORDER BY check_date, pool_name, volume_name "
-
-curs.execute(SQL)
-RESULT = curs.fetchall()
-
-curs.execute(SQL_2)
-RESULT_2 = curs.fetchall()
-
-conn.close()
-
-TableName = "EventLog"
-Columns = "backup_dt,schedule_tm,schedule_nm,nodename,domain_nm,start_tm,end_tm,taken,status,returncode,action,note,modify_dt"
-
 TableName_2 = "RemoteKeep"
 Columns_2 = "check_dt, volume_nm, pool_nm, cycle, due_dt, safein_chk, safeout_chk"
+INSQL_2 = f"INSERT INTO {TableName_2} ({Columns_2}) VALUES (%s, %s, %s, %s, %s, 'X', 'X')"
 
-for VALUES in list(RESULT):
 
-    conn = pymysql.connect(host='localhost', user='root', password='xmflrj', db='myDjango', port=3456, charset='utf8')
+#conn = pymysql.connect(host='192.168.15.11', user='root', password='xmflrj', db='tsmBackup', charset='utf8')
+conn = pymysql.connect(host='180.70.98.216', user='root', password='collect123$', db='tsmbackup', charset='utf8')
+curs = conn.cursor()
+for i in range(1,3):
+    curs.execute(globals()[f'SQL_{i}'])
+    globals()[f'RESULT_{i}'] = curs.fetchall()
+conn.close()
 
-    try:
-        with conn.cursor() as cursor:
-            SQL = f"INSERT INTO {TableName} ({Columns}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, '', '',now())"
-            print (SQL)
-            cursor.execute(SQL, VALUES)
 
-        conn.commit()
+for i in range(1,3):
+    for VALUES in list(globals()[f'RESULT_{i}']):
+        conn = pymysql.connect(host='localhost', user='root', password='xmflrj', db='myDjango', port=3456, charset='utf8')
 
-    except pymysql.err.IntegrityError as e :
-        print ("Error: {}".format(e))
+        try:
+            with conn.cursor() as cursor:
+                print (globals()[f'INSQL_{i}'])
+                cursor.execute(globals()[f'INSQL_{i}'], VALUES)
 
-#    except MySQLdb.Error as e :
-#        pass
+            conn.commit()
 
-    finally:
-        conn.close()
+        except pymysql.err.IntegrityError as e :
+            print ("Error: {}".format(e))
 
-for VALUES in list(RESULT_2):
-    
-    conn = pymysql.connect(host='localhost', user='root', password='xmflrj', db='myDjango', port=3456, charset='utf8')
+    #    except MySQLdb.Error as e :
+    #        pass
 
-    try:
-        with conn.cursor() as cursor:
-            SQL = f"INSERT INTO {TableName_2} ({Columns_2}) VALUES (%s, %s, %s, %s, %s, 'X', 'X')"
-            print (SQL)
-            cursor.execute(SQL, VALUES)
-
-        conn.commit()
-
-    except pymysql.err.IntegrityError as e :
-        print ("Error: {}".format(e))
-
-#    except MySQLdb.Error as e :
-#        pass
-
-    finally:
-        conn.close()
+        finally:
+            conn.close()
