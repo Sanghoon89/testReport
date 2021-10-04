867ace45be3cee5a6048dd7a2a377d5bbb875712
Hooni <lmsh2355@naver.com>
2021-10-05 00:12:09 +0900

    remote admin

--- Remote/admin.py
+++ Remote/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from Event.models import Keep
 
 @admin.register(Keep)
-class LogAdmin(admin.ModelAdmin):
+class KeepAdmin(admin.ModelAdmin):
     list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk') # 변경
     list_filter = ('safein_chk',)
     search_fields = ('check_dt', 'volume_nm')
