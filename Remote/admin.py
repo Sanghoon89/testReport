44ee9b6f1b64294683a8fac1712b893d8ddb837b
Hooni <lmsh2355@naver.com>
2021-10-05 00:21:05 +0900

    remote admin

--- Remote/admin.py
+++ Remote/admin.py
@@ -6,6 +6,6 @@ from Remote.models import Keep
 
 @admin.register(Keep)
 class KeepAdmin(admin.ModelAdmin):
-    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk') # 변경
-    list_filter = ('safein_chk',)
+    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk',) # 변경
+    list_filter = ('safein_chk','safeout_chk')
     search_fields = ('check_dt', 'volume_nm')
