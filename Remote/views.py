139fcd6d18f78dacac17b1056d344e041e7b5a17
Hooni <lmsh2355@naver.com>
2021-10-04 23:44:36 +0900

    test

--- Remote/views.py
+++ Remote/views.py
@@ -14,7 +14,7 @@ YESTER = date.today() - timedelta(1)
 YESTERDAY = YESTER.strftime('%Y-%m-%d')
 
 YESTERDAY="2021-09-08"
-TODAY="2021-09-11"
+TODAY="2021-09-14"
 class KeepLV(ListView):
     model = Keep
     template_name = 'Remote/main.html'
