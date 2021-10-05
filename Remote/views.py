e68ff74311822a10388a6b5ab60f0cd8d66165d9
문상훈 <hooni@interpark.com>
2021-10-05 12:57:27 +0900

    remote views

--- Remote/views.py
+++ Remote/views.py
@@ -20,12 +20,13 @@ class KeepLV(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(KeepLV, self).get_context_data(**kwargs)
-        imsi = Keep.objects.filter(due_dt__lte=TODAY, safein_chk='O', safeout_chk='X') | Keep.objects.filter(due_dt__lte=TODAY, safein_chk='O', safeout_chk='O',safeout_dt=TODAY)
+        imsi = Keep.objects.filter(due_dt__lte=TODAY,safein_chk='O',safeout_chk='X') | Keep.objects.filter(due_dt__lte=TODAY,safein_chk='O',safeout_chk='O',safeout_dt=TODAY)
         context['checkin'] = imsi.order_by('-cycle', 'due_dt', 'volume_nm')
         imsi = Keep.objects.filter(check_dt=YESTERDAY)
         context['checkout'] = imsi.order_by('-cycle', 'due_dt', 'volume_nm')
         imsi = Keep.objects.filter(pool_nm='srcpool', due_dt__gt=TODAY)
         context['srcpool'] = imsi.order_by('due_dt')
+        context['today'] = TODAY
 
         return context
 
