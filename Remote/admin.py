63dfe2fe0301bad1954c0cfcf2c8af7618d2dee8
문상훈 <hooni@interpark.com>
2021-10-14 13:11:33 +0900

    remote admin

--- Remote/admin.py
+++ Remote/admin.py
@@ -6,7 +6,8 @@ from Remote.models import Keep
 
 @admin.register(Keep)
 class KeepAdmin(admin.ModelAdmin):
-    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk',) # 변경
+    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',) # 변경
     list_display_link = ('check_dt', 'volume_nm',)
+    list_editable = ('safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',)
     list_filter = ('safein_chk','safeout_chk')
     search_fields = ('check_dt', 'volume_nm')
