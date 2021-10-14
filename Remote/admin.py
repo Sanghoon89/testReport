5fdb74fe8a2f365543b694d5e47f169b36c1268a
Hooni <lmsh2355@naver.com>
2021-10-14 20:56:42 +0900

    remote admin model

--- Remote/admin.py
+++ Remote/admin.py
@@ -6,7 +6,7 @@ from Remote.models import Keep
 
 @admin.register(Keep)
 class KeepAdmin(admin.ModelAdmin):
-    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',) # 변경
+    list_display = ('check_dt', 'volume_nm', 'due_dt', 'safein_chk', 'safeout_chk','safein_dt', 'safeout_dt', 'pool_nm', 'cycle',) # 변경
     list_display_links = ('check_dt', 'volume_nm',)
     list_editable = ('safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',)
     list_filter = ('safein_chk','safeout_chk')
--- Remote/models.py
+++ Remote/models.py
@@ -3,12 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Keep(models.Model):
+    safein_CHOICES = (
+        ('o', '입고확인'),
+        ('x', '입고대기'),
+        ('w', 'Withdrawn')
+    )
     check_dt = models.DateField('Check-out Date')
     volume_nm = models.CharField('Tape Volume Name', max_length=20)
     pool_nm = models.CharField('Pool Name', max_length=20)
     cycle = models.IntegerField('Keep Cycle', default=14)
     due_dt = models.DateField('Due Date')
-    safein_chk = models.CharField('Check in coffer', max_length=1, default="X")
+    safein_chk = models.CharField('Check in coffer', max_length=1, default="X", choices=STATUS_CHOICES)
     safein_dt = models.DateField('Safe-in Date', null=True, blank=True)
     safeout_chk = models.CharField('Check out coffer', max_length=1, default="X")
     safeout_dt = models.DateField('Safe-out Date', null=True, blank=True)
