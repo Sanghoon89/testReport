f5fbd9907b0e00457f54fc4485e1a0a9b85c5694
Hooni <lmsh2355@naver.com>
2021-10-15 00:25:52 +0900

    remote admin

--- Remote/admin.py
+++ Remote/admin.py
@@ -9,7 +9,7 @@ from django.db.models import F
 
 @admin.register(Keep)
 class KeepAdmin(admin.ModelAdmin):
-    list_display = ('check_dt', 'volume_nm', 'pool_nm', 'cycle', 'due_dt', 'safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',) # 변경
+    list_display = ('check_dt','volume_nm','pool_nm','cycle','due_dt','safein_chk','safein_dt','safeout_chk','safeout_dt',) # 변경
     list_display_links = ('check_dt', 'volume_nm',)
     list_editable = ('safein_chk', 'safeout_chk','safein_dt', 'safeout_dt',)
     list_filter = ('safein_chk','safeout_chk')
