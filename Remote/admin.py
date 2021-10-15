82b061181f8463b73dc46aba3f5714eabf8f8bb2
Hooni <lmsh2355@naver.com>
2021-10-16 08:28:06 +0900

    remote admin

--- Remote/admin.py
+++ Remote/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-
 from Remote.models import Keep
 #from django.utils.safestring import mark_safe
 from datetime import datetime, date
