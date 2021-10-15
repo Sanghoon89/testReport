b6bd66f04baf69db3b28f3d06f110803ea9a098e
문상훈 <hooni@interpark.com>
2021-10-15 09:56:18 +0900

    event admin

--- Event/admin.py
+++ Event/admin.py
@@ -7,5 +7,7 @@ from Event.models import Log
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     list_display = ('backup_dt', 'schedule_tm', 'schedule_nm', 'nodename', 'domain_nm', 'start_tm', 'end_tm', 'taken', 'status', 'returncode', 'action', 'note') # 변경
-    list_filter = ('returncode',)
-    search_fields = ('schedule_nm', 'nodename')
+    list_display_links = ('backup_dt', 'schedule_nm',)
+    list_editable = ('status', 'action', 'note',)
+    list_filter = ('status', 'returncode',)
+    search_fields = ('schedule_nm', 'nodename', 'status', 'returncode')
