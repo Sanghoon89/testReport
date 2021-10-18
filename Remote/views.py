c2791d3b462dab2acfe40d525899aefc3dfabede
문상훈 <hooni@interpark.com>
2021-10-18 09:44:30 +0900

    remote view

--- Remote/views.py
+++ Remote/views.py
@@ -7,25 +7,28 @@ from Remote.models import Keep
 
 from datetime import datetime, date, timedelta
 import locale
-## 날짜
-Delta=1
+# 날짜
+Delta = 1
 if datetime.today().weekday() == 0:
-    Delta=3
+    Delta = 3
 
-locale.setlocale(locale.LC_ALL,'')
+locale.setlocale(locale.LC_ALL, '')
 TODAY = date.today().strftime('%Y-%m-%d')
 YESTER = date.today() - timedelta(Delta)
 YESTERDAY = YESTER.strftime('%Y-%m-%d')
 
+
 class KeepLV(ListView):
     model = Keep
-    template_name = 'Remote/main_2.html'
+    template_name = 'Remote/main.html'
     context_object_name = 'keeps'
 
     def get_context_data(self, **kwargs):
         context = super(KeepLV, self).get_context_data(**kwargs)
-        imsi1 = Keep.objects.filter(due_dt__lte=TODAY,safein_chk='o',safeout_chk='x')
-        imsi2 = Keep.objects.filter(due_dt__lte=TODAY,safein_chk='o',safeout_chk='o',safeout_dt=TODAY)
+        imsi1 = Keep.objects.filter(
+            due_dt__lte=TODAY, safein_chk='o', safeout_chk='x')
+        imsi2 = Keep.objects.filter(
+            due_dt__lte=TODAY, safein_chk='o', safeout_chk='o', safeout_dt=TODAY)
         imsi = imsi1 | imsi2
         context['checkin'] = imsi.order_by('-cycle', 'due_dt', 'volume_nm')
         context['today'] = imsi.order_by('-due_dt')
@@ -36,5 +39,5 @@ class KeepLV(ListView):
 
         return context
 
-    def __str__ (self):
+    def __str__(self):
         return self
